b1fbbc4423be1c64c74d61fbc25fda79739cc0ca
Math Olvt <47877475+mamazinho@users.noreply.github.com>
2019-10-09 01:40:06 -0300

    Update conreq.py

--- conreq.py
+++ conreq.py
@@ -19,17 +19,29 @@ else:
     print("Not_Connected")
 
 #Testing Cursor
-query = "SELECT * FROM channel;"
-cur.execute(query)
+#query = "SELECT * FROM channel;"
+#cur.execute(query)
 
-for cha in cur:
-    print(cha)
+#for cha in cur:
+#    print(cha)
 
 #Test Request
 url = "http://ela.oglobo.globo.com"
 resp = requests.get(url)
 
 print(resp.url)
+
+def func(acc):
+
+    query = "SELECT chaId, chaurl FROM channel WHERE accId = " + str(acc) + ";"
+    cur.execute(query)
+    for fetch in cur:
+        print(fetch)
+
+#acc = input("Passe o id da conta: ") 
+#func(acc)
+func(input("Passe o id da conta: "))
+
 
 #Close the Connection 
 cur.close()
